01135d83e8d3755cab08549e10e49ed200ed3872
Palekar Sumit Namdev <23je0679@iitism.ac.in>
2026-01-05 00:03:56 +0530

    Delete frontend/src/lib directory

--- frontend/src/lib/erc20.js
+++
@@ -1,6 +0,0 @@
-import { ethers } from "ethers";
-import ERC20ABI from "@/abis/ERC20.json";
-
-export function getERC20(tokenAddress, signerOrProvider) {
-  return new ethers.Contract(tokenAddress, ERC20ABI, signerOrProvider);
-}
